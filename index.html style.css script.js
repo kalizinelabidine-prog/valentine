e150ea4edac012d9444b8c03873faa5ba61848ae
kalizinelabidine-prog <kalizinelabidine@gmail.com>
2026-02-01 20:21:29 +0100

    Create index.html style.css script.js
    Zin

---
+++ index.html style.css script.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Be My Valentine 💖</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <div class="container">
+    <h1>Hey there 💕</h1>
+    <h2>Will you be my Valentine?</h2>
+
+    <div class="buttons">
+      <button id="yes">YES OF COURSE 💖</button>
+      <button id="no">NO THANK YOU</button>
+    </div>
+  </div>
+
+  <script src="script.js"></script>
+</body>
+</html>
